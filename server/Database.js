2e1f83552c77a9b7012b2af4e84fbbca0f270070
Arseniy Sidorov <arseniy.sidorov2013@gmail.com>
2022-05-04 20:17:42 +0500

    feat: implemented api requests to reports

--- server/Database.js
+++ server/Database.js
@@ -10,35 +10,91 @@ export default class Database {
     // reports
 
     // create
-    createData(title, fields_to_insert) {
-
+    postReport(requestData) {
+        const { title, maintext, tags, filenames } = requestData;
+        return new Promise((resolve, reject) => {
+            const date = new Date().toISOString().split('T')[0].split('-');
+            const parsedDate = `${date[2]}-${date[1]}-${date[0]}`;
+            this.db.run('INSERT INTO reports (title, description, images_links, tags, date_created, status)' +
+                `VALUES ('${title}', '${maintext}', '${filenames.join(',')}', '${tags}', '${parsedDate}', 'Todo')`, (err) => {
+                    if(err) {
+                        reject(err);
+                    }
+                    resolve();
+                })
+        });
     }
     // read
-    getDataById(id) {
-
+    getReportById(id) {
+        return new Promise((resolve, reject) => {
+            this.db.get(`SELECT report_id, title, date_created, description, images_links, tags, status FROM reports WHERE report_id = ${id}`, (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                resolve(row);
+            });
+        });
     }
-    getShortDataById(id) {
-
+    getImagesListById(id) {
+        return new Promise((resolve, reject) => {
+            this.db.get(`SELECT images_links FROM reports WHERE report_id = ${id}`, (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                resolve(row);
+            });
+        });
     }
-    getShortDataList(limit = 50, offset = 0) {
+    getShortReports(limit = 50, offset = 0) {
         return new Promise((resolve, reject) => {
             this.db.all(`SELECT report_id, title, date_created, tags, status FROM reports LIMIT ${limit} OFFSET ${offset}`, (err, rows) => {
-                console.log(err);
-                console.log(rows);
+                if(err) {
+                    reject(err);
+                }
                 resolve(rows);
             });
         });
     }
-    getDataList(limit = 50, offset = 0) {
-
+    getReports(limit = 50, offset = 0) {
+        return new Promise((resolve, reject) => {
+            this.db.all(`SELECT report_id, title, date_created, tags, status FROM reports LIMIT ${limit} OFFSET ${offset}`, (err, rows) => {
+                if(err) {
+                    reject(err);
+                }
+                resolve(rows);
+            });
+        });
     }
     // update
     updateData(id, fields_to_update) {
+        const { title, description, tags, status } = fields_to_update;
 
+        const arrayOfStrings = [];
+        
+        title && arrayOfStrings.push(`title = '${title}'`);
+        description && arrayOfStrings.push(`description = '${description}'`);
+        tags && arrayOfStrings.push(`tags = '${tags}'`);
+        status && arrayOfStrings.push(`status = '${status}'`);
+        
+        return new Promise((resolve, reject) => {
+            this.db.run(`UPDATE reports SET ${arrayOfStrings.join(',')} WHERE report_id = ${id}`, (err) => {
+                    if(err) {
+                        reject(err);
+                    }
+                    resolve();
+                })
+        });
     }
     // delete
     deleteData(id) {
-
+        return new Promise((resolve, reject) => {
+            this.db.run(`DELETE FROM reports WHERE report_id = ${id}`, (err) => {
+                    if(err) {
+                        reject(err);
+                    }
+                    resolve();
+                })
+        });
     }
     // users
     getUser(login, password) {
--- server/index.js
+++ server/index.js
@@ -1,55 +1,108 @@
 import express from 'express';
-import Database from './Database.js';
-
 const app = express();
+import Database from './Database.js';
+import path from 'path';
+import multer from 'multer';
+import fs from 'fs';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + Date.now() + path.extname(file.originalname))
+    }
+});
+   
+const upload = multer({ storage: storage })
 
 
 const db = new Database('./records.db');
 
 app.use(express.json());
 
+app.post('/reports', upload.array('images', 3), (req, res) => {
+    const { title, maintext, tags } = req.body;
+    const filenames = req.files.map(el => el.filename);
 
-app.post('createData', (req, res) => {
-
+    const requestData = { title, maintext, tags, filenames };
+    
+    db.postReport(requestData).then((value) => {
+        res.end();
+    }).catch((reason) => {
+        res.status(500).send(reason);
+    });
 });
 
-// PARAMS
-// short: boolean
-// id: number
-app.get('getDataById', (req, res) => {
-
-});
-// PARAMS
-// short: boolean
-// offset: number
-// limit: number < 50
-app.get('/getData', (req, res) => {
-    if(req.query.short) {
-        const limit = req.query.limit;
-        const offset = req.query.offset;
-        db.getShortDataList(limit, offset).then((value) => {
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.send(value);
-        });
+app.get('/reports', (req, res) => {
+    const { offset, limit, short } = req.query;
+    let promise;
+    if(short) {
+        promise = db.getShortReports(limit, offset);
+    } else {
+        promise = db.getReports(limit, offset);
     }
-});
-// PARAMS
-// text: string
-app.get('getDataByValue', (req, res) => {
-
-});
-
-app.post('updateData', () => {
-
-});
-
-app.post('deleteData', () => {
-
+    promise.then((value) => {
+        value.map(el => {
+            el.images_count = el.images_links && el.images_links.split(',').length;
+            delete el.images_links
+        });
+        res.send(value);
+    }).catch((reason) => {
+        res.status(500).send(reason);
+    });
 });
 
-app.use('/auth', (req, res) => {
+app.get('/reports/:id', (req, res) => {
+    const { id } = req.params;
+    db.getReportById(id).then((el) => {
+        
+        el.images_count = el.images_links && el.images_links.split(',').length;
+        delete el.images_links
+
+        res.send(value);
+    }).catch((reason) => {
+        res.status(500).send(reason);
+    });
+})
+
+app.put('/reports/:id', (req, res) => {
+    const { id } = req.params;
+    db.updateData(id, req.query).then(() => {
+        res.end();
+    }).catch((reason) => {
+        res.status(500).send(reason);
+    });
+});
+
+app.delete('/reports/:id', (req, res) => {
+    const { id } = req.params;
+    db.getImagesListById(id).then((row) => {
+        if(row.images_links) {
+            row.images_links.split(',').map(name => {
+                fs.unlink("./uploads/" + name, (err) => {
+                    if(err) {
+                        res.status(500).send('Error! The image is not found!');
+                        return;
+                    }
+                });
+            })
+        }
+        db.deleteData(id).then(() => {
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.end();
+        }).catch((reason) => {
+            res.status(500).send(reason);
+        });
+    })
+
 
 });
+
+app.get('/img/id', (req, res) => {
+    const { img_name } = req.params;
+    
+})
 
 app.listen(1000, () => {
     console.log('listening to 1000');
