60078fbbb341f29310fc338652fe68ef2af2d1fe
Arseniy Sidorov <arseniy.sidorov2013@gmail.com>
2022-06-12 14:21:06 +0500

    feat: database refactor

--- server/Database.js
+++ server/Database.js
@@ -7,63 +7,53 @@ export default class Database {
         this.db = new sqllite3.Database(path);
     }
 
+    runQuery = (query) => {
+        return new Promise((resolve, reject) => {
+            this.db.run(query, (err) => {
+                if(err) { reject(err) }
+                resolve();
+            })
+        })
+    }
+    getQuery = (query) => {
+        return new Promise((resolve, reject) => {
+            this.db.get(query, (err, row) => {
+                if(err) { reject(err) }
+                resolve(row);
+            })
+        })
+    }
+    allQuery = (query) => {
+        return new Promise((resolve, reject) => {
+            this.db.all(query, (err, rows) => {
+                if(err) { reject(err) }
+                resolve(rows);
+            })
+        })
+    }
+
     // reports
 
     // create
     postReport(requestData) {
         const { title, maintext, tags, filenames } = requestData;
-        return new Promise((resolve, reject) => {
-            const date = new Date().toISOString().split('T')[0].split('-');
-            const parsedDate = `${date[2]}-${date[1]}-${date[0]}`;
-            this.db.run('INSERT INTO reports (title, description, images_links, tags, date_created, status)' +
-                `VALUES ('${title}', '${maintext}', '${filenames.join(',')}', '${tags}', '${parsedDate}', 'Todo')`, (err) => {
-                    if(err) {
-                        reject(err);
-                    }
-                    resolve();
-                })
-        });
+        const date = new Date().toISOString().split('T')[0].split('-');
+        const parsedDate = `${date[2]}-${date[1]}-${date[0]}`;
+        return this.runQuery('INSERT INTO reports (title, description, images_links, tags, date_created, status)' +
+        `VALUES ('${title}', '${maintext}', '${filenames.join(',')}', '${tags}', '${parsedDate}', 'Todo')`);
     }
     // read
     getReportById(id) {
-        return new Promise((resolve, reject) => {
-            this.db.get(`SELECT report_id, title, date_created, description, images_links, tags, status FROM reports WHERE report_id = ${id}`, (err, row) => {
-                if(err) {
-                    reject(err);
-                }
-                resolve(row);
-            });
-        });
+        return this.getQuery(`SELECT report_id, title, date_created, description, images_links, tags, status FROM reports WHERE report_id = ${id}`);
     }
     getImagesListById(id) {
-        return new Promise((resolve, reject) => {
-            this.db.get(`SELECT images_links FROM reports WHERE report_id = ${id}`, (err, row) => {
-                if(err) {
-                    reject(err);
-                }
-                resolve(row);
-            });
-        });
+        return this.getQuery(`SELECT images_links FROM reports WHERE report_id = ${id}`);
     }
     getShortReports(limit = 50, offset = 0) {
-        return new Promise((resolve, reject) => {
-            this.db.all(`SELECT report_id, title, date_created, tags, status FROM reports LIMIT ${limit} OFFSET ${offset}`, (err, rows) => {
-                if(err) {
-                    reject(err);
-                }
-                resolve(rows);
-            });
-        });
+        return this.allQuery(`SELECT report_id, title, date_created, tags, status FROM reports LIMIT ${limit} OFFSET ${offset}`);
     }
     getReports(limit = 50, offset = 0) {
-        return new Promise((resolve, reject) => {
-            this.db.all(`SELECT report_id, title, date_created, tags, status FROM reports LIMIT ${limit} OFFSET ${offset}`, (err, rows) => {
-                if(err) {
-                    reject(err);
-                }
-                resolve(rows);
-            });
-        });
+        return this.allQuery(`SELECT report_id, title, date_created, tags, status FROM reports LIMIT ${limit} OFFSET ${offset}`);
     }
     // update
     updateData(id, fields_to_update) {
@@ -76,61 +66,27 @@ export default class Database {
         tags && arrayOfStrings.push(`tags = '${tags}'`);
         status && arrayOfStrings.push(`status = '${status}'`);
         
-        return new Promise((resolve, reject) => {
-            this.db.run(`UPDATE reports SET ${arrayOfStrings.join(',')} WHERE report_id = ${id}`, (err) => {
-                    if(err) {
-                        reject(err);
-                    }
-                    resolve();
-                })
-        });
+        return this.runQuery(`UPDATE reports SET ${arrayOfStrings.join(',')} WHERE report_id = ${id}`);
     }
     // delete
     deleteData(id) {
-        return new Promise((resolve, reject) => {
-            this.db.run(`DELETE FROM reports WHERE report_id = ${id}`, (err) => {
-                    if(err) {
-                        reject(err);
-                    }
-                    resolve();
-                })
-        });
+        return this.runQuery(`DELETE FROM reports WHERE report_id = ${id}`);
     }
 
     // tokens
     getTokens() {
-        return new Promise((resolve, reject) => {
-            this.db.all(`SELECT token_id, token FROM tokens LEFT JOIN users ON tokens.token_id = users.user_id`, (err, rows) => {
-                if(err) {
-                    reject(err);
-                }
-                resolve(rows);
-            })
-        })
+        return this.allQuery(`SELECT token_id, token FROM tokens LEFT JOIN users ON tokens.token_id = users.user_id`);
     }
 
 
     // tags
     getTags() {
-        return new Promise((resolve, reject) => {
-            this.db.all('SELECT * FROM tags', (err, rows) => {
-                if(err) {
-                    reject(err);
-                }
-                resolve(rows);
-            })
-        })
+        return this.allQuery('SELECT * FROM tags');
     }
 
     // users
     getUser(login, password) {
-        return new Promise((resolve, reject) => {
-            this.db.get(`SELECT * FROM users WHERE login="${login}" AND password="${password}"`, (err, row) => {
-                    if(err) {
-                        reject(err);
-                    }
-                    resolve(row);
-                })
-        });    }
+        return this.getQuery(`SELECT * FROM users WHERE login="${login}" AND password="${password}"`);
+    }
 
 }
